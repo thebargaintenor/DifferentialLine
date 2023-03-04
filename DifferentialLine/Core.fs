d83c501620770cbad54a002f5fe727afc1042340
Ted Monchamp <ted.monchamp@gmail.com>
2023-03-03 22:58:18 -0500

    more maths

--- DifferentialLine/Core.fs
+++ DifferentialLine/Core.fs
@@ -4,20 +4,20 @@ open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 
 module VecMath =
-    let ScaleToMagnitude (magnitude: float) (vector: Vector<float>): Vector<float> =
+    let scaleToMagnitude (magnitude: float) (vector: Vector<float>): Vector<float> =
         vector.Multiply(magnitude / vector.L2Norm())
 
-    let Add (a: Vector<'a>) (b: Vector<'a>): Vector<'a> = b.Add(a)
+    let add (a: Vector<'a>) (b: Vector<'a>): Vector<'a> = b.Add(a)
 
-    let Subtract (a: Vector<'a>) (b: Vector<'a>): Vector<'a> = b.Subtract(a)
+    let subtract (a: Vector<'a>) (b: Vector<'a>): Vector<'a> = b.Subtract(a)
 
-    let MultiplyScalar (a: 'a) (b: Vector<'a>): Vector<'a> = b.Multiply(a)
+    let multiplyScalar (a: 'a) (b: Vector<'a>): Vector<'a> = b.Multiply(a)
 
-    let DivideScalar (a: 'a) (b: Vector<'a>): Vector<'a> = b.Divide(a)
+    let divideScalar (a: 'a) (b: Vector<'a>): Vector<'a> = b.Divide(a)
 
-    let Limit (max: float) (vector: Vector<float>): Vector<float> =
+    let limit (max: float) (vector: Vector<float>): Vector<float> =
         if vector.L2Norm() > max then
-            ScaleToMagnitude max vector
+            scaleToMagnitude max vector
         else
             vector
     
@@ -27,7 +27,9 @@ module VecMath =
 module Core =
     type Node = 
         { position: Vector<float>; 
-          velocity: Vector<float>; }
+          velocity: Vector<float>; 
+          fmax: float;
+          vmax: float; }
 
     type DifferentialLine =
         { nodes: Node array
@@ -37,19 +39,21 @@ module Core =
           cohesionRatio: float
           maxEdgeLength: float }
 
+    let origin = DenseVector.create 2 0.0
+
     let separationForce (cutoffDistance: float) (a: Node) (b: Node): Vector<float> =
         let dist = Distance.Euclidean(a.position, b.position)
         if dist > 0 && dist <= cutoffDistance then
             a.position.Subtract(b.position)
             |> VecMath.Normalize
-            |> VecMath.DivideScalar dist
+            |> VecMath.divideScalar dist
         else
             DenseVector.create 2 0.0
 
     // this will have lots of opportunities for optimization
     let separationForces (dl: DifferentialLine) : Vector<float> array =
         let n = dl.nodes.Length
-        let forces = Array.init n (fun i -> DenseVector.create 2 0.0)
+        let forces = Array.init n (fun i -> origin)
         let nearNodes = Array.zeroCreate<int> n
 
         for i = 0 to n-1 do
@@ -66,13 +70,31 @@ module Core =
 
                 if forces.[i].L2Norm() > 0 then
                     forces.[i] <- 
-                        VecMath.ScaleToMagnitude dl.maxSpeed forces.[i]
-                        |> VecMath.Subtract dl.nodes.[i].velocity
-                        |> VecMath.Limit dl.maxForce
+                        VecMath.scaleToMagnitude dl.maxSpeed forces.[i]
+                        |> VecMath.subtract dl.nodes.[i].velocity
+                        |> VecMath.limit dl.maxForce
         forces
 
+    let attract (node: Node) (target: Vector<float>): Vector<float> =
+        target
+        |> VecMath.subtract node.position
+        |> VecMath.scaleToMagnitude node.vmax
+        |> VecMath.subtract node.velocity
+        |> VecMath.limit node.fmax
+
+
     let cohesionForces (dl: DifferentialLine) : Vector<float> array =
-        dl.nodes |> Array.map (fun n -> DenseVector.create 2 0.0)
+        let nmax = dl.nodes.Length - 1
+        
+        [| 0 .. nmax |]
+        |> Array.map (fun i ->
+            match i with
+            | 0 -> dl.nodes.[nmax].position.Add(dl.nodes.[1].position)
+            | n when n = nmax -> dl.nodes.[n-1].position.Add(dl.nodes.[0].position)
+            | _ -> dl.nodes.[i-1].position.Add(dl.nodes.[i+1].position)
+            )
+        |> Array.map (VecMath.divideScalar 2.0)
+        |> Array.map2 attract dl.nodes
 
     /// <summary>Calculates the changes in acceleration for each node
     /// in the given differential line path.
@@ -83,6 +105,5 @@ module Core =
     /// <returns>List of 2D acceleration deltas</returns>
     let differentiate (dl: DifferentialLine) : Vector<float> array =
         separationForces dl
-        |> Array.map (fun v -> v.Multiply dl.cohesionRatio)
-        |> Array.zip (cohesionForces dl)
-        |> Array.map (fun (a, b) -> a.Add(b))
+        |> Array.map (VecMath.multiplyScalar dl.cohesionRatio)
+        |> Array.map2 VecMath.add (cohesionForces dl)
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ One day, maybe an interactive generative art widget.  Once the computational bit
 * [Differential Line Growth with Processing](http://www.codeplastic.com/2017/07/22/differential-line-growth-with-processing/)
 * [Inconvergent](https://inconvergent.net/generative/differential-line/)
 * [Differential Line Growth: The Quick Way](https://www.youtube.com/watch?v=SOk5qVXrFQA)
+* This [post](https://medium.com/@jason.webb/2d-differential-growth-in-js-1843fd51b0ce) by Jason Webb
+  
